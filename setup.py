7e50e7ed6616cbd87b2074060bbeb2e9d682b69a
J. S. Kottmann <jakob.kottmann@gmail.com>
2021-12-01 15:44:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="1.0",
     author='Jakob S. Kottmann',
     author_email='jakob.kottmann@gmail.com',
-    install_requires=[],
+    install_requires=[tequila-basic],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
